f61f619299b77b4a649f918d9440cefda7504c25
João Vitor Estevam <joaophvt@gmail.com>
2023-05-27 22:40:48 -0300

    desafio podekex dio

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocadeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro");
+
+    if (modoEscuroEstaAtivo) {
+        imagemBotaoTrocadeTema.setAttribute("src", "./assets/imgs/moon.png");
+    } else {
+        imagemBotaoTrocadeTema.setAttribute("src", "./assets/imgs/sun.png");
+    }
+});
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
